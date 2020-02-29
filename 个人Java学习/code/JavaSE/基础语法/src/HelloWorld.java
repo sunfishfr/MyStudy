d5d57a24abe857b8d75409a29ee55a5f804b8d1a
luhan <719358293@qq.com>
2020-02-29 14:22:30 +0800

    fist commit

---
+++ 个人Java学习/code/JavaSE/基础语法/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "luhan";
+        System.out.println("Hello World~");
+        int a = 0b101;
+        System.out.println(a);
+    }
+}
