29da75be6eea71e4aa25407b6cf6baa5e65a31b0
luhan <719358293@qq.com>
2020-03-01 00:44:35 +0800

    Java基础

--- 个人Java学习/code/JavaSE/基础语法/src/HelloWorld.java
+++ 个人Java学习/code/JavaSE/基础语法/src/HelloWorld.java
@@ -1,8 +1,23 @@
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         String name = "luhan";
         System.out.println("Hello World~");
         int a = 0b101;
-        System.out.println(a);
+        System.out.println((byte)128);
+
+        int money = 10_0000_0000;
+        System.out.println(money);
+        int year = 20;
+        int total1 = money * year;
+        System.out.println(total1);//输出：-1474836480
+        //尝试改变
+        long total2 = money * year;
+        System.out.println(total2);//输出：-1474836480
+        //再次调整
+        long total3 = money * (long)year;
+        System.out.println(total3);//输出：20000000000
+
     }
 }
---
+++ 个人Java学习/code/JavaSE/基础语法/src/com/luhan/doc/Doc.java
@@ -0,0 +1,20 @@
+package com.luhan.doc;
+
+/**
+ * @author luhan
+ * @version 1.0
+ * @since 1.8
+ */
+public class Doc {
+    String name;
+
+    /**
+     *
+     * @param name
+     * @return
+     * @throws Exception
+     */
+    public String test(String name) throws Exception{
+        return name;
+    }
+}
